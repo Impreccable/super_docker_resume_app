504d9c3ccdf1c031f1571066c3dab0e2d31d283d
Impreccable <evvlasov1990@gmail.com>
2024-04-16 16:18:44 +0200

    add contact form

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, render_template, redirect, url_for
 from extensions import app, db
-
+from datetime import datetime
 from models import *
 
 @app.route('/')
@@ -11,8 +11,22 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        try:
+            name = request.form['name']
+            email = request.form['email']
+            text = request.form['text']
+            
+            ip = request.remote_addr
+            created_at = datetime.now()
+
+            DB_Contact.insert_contact(created_at, ip, name, email, text, phone=None)
+            
+            return redirect(url_for('contacted'))
+        except Exception as e:
+            return f"An error occurred: {e}"
     return render_template('contact.html')
 
 @app.route('/contacted')
